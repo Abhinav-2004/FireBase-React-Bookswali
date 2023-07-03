3946ac3e6ae60eeb93c4e858eb0fe4e5c6500af1
Abhinav Singh <kittusinghranchi@gmail.com>
2023-07-03 17:04:39 +0530

    day2 completed
    all login logout working with navbar showing login when logged out and vice versa

--- day1/src/Components/Home.js
+++ day1/src/Components/Home.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import Navbar from './Navbar';
-import Products from './Products'
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import Products from "./Products";
+import Banner from "./Banner";
+import { auth, db } from "../FirebaseConfig/FirebaseConfig";
+import { collection, getDocs, query, where } from "firebase/firestore";
 const Home = () => {
+  function GetCurrentUser() {
+    const [user, setUser] = useState("");
+    const userCollectionRef = collection(db, "users");
+    useEffect(() => {
+      auth.onAuthStateChanged(userlogged => {
+        if (userlogged) {
+          const getUsers = async () => {
+            const q = query(collection(db, "users"),
+              where("uid", "==", userlogged.uid)
+            );
+            const data = await getDocs(q);
+            //console.log(q);
+            setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+          };
+          getUsers();
+        } else {
+          setUser(null);
+        }
+      });
+    }, []);
+    return user;
+  }
+  const loggedUser = GetCurrentUser();
+  if(loggedUser){console.log(loggedUser[0].email)}
   return (
     <div>
-      <Navbar/>
-      <Products/>
+      <Navbar />
+      <Banner />
+      <Products />
+      <p>{loggedUser?loggedUser[0].email:"User not found"}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- day1/src/Components/Navbar.js
+++ day1/src/Components/Navbar.js
@@ -1,25 +1,79 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import './Navbar.css';
 import Cartlogo from './assets/cartlogo.png';
 import Profilelogo from './assets/profilelogo.png';
+import appLogo from './assets/Booklogo.png';
+import { auth,db } from '../FirebaseConfig/FirebaseConfig';
+import {collection,getDocs,query,where} from 'firebase/firestore';
+import { useEffect,useState } from 'react';
 const Navbar = () => {
+  function GetCurrentUser() {
+    const [user, setUser] = useState("");
+    const userCollectionRef = collection(db, "users");
+    useEffect(() => {
+      auth.onAuthStateChanged(userlogged => {
+        if (userlogged) {
+          const getUsers = async () => {
+            const q = query(collection(db, "users"),
+              where("uid", "==", userlogged.uid)
+            );
+            const data = await getDocs(q);
+            //console.log(q);
+            setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+          };
+          getUsers();
+        } else {
+          setUser(null);
+        }
+      });
+    }, []);
+    return user;
+  }
+
+  const navigate=useNavigate();
+  const loggedUser=GetCurrentUser();
+  const handleLogout=()=>{
+    auth.signOut().then(()=>{
+      navigate('/login');
+    })
+  }
   return (
-    <div>
-      <nav>
-        <Link to = '/home'><button>Home</button> </Link>
-        <Link to = '/signup'><button>Signup</button> </Link>
-        <Link to = '/login'><button>Login</button> </Link>
-        <Link to='/cart'>
-        <div className='cart-btn'>
-            <img src = {Cartlogo} alt =""/>
+    <div className='navbar'>
+      <div className='left-container'>
+        <img src={appLogo}/>
+      </div>
+      <div className='right-container'>
+        {!loggedUser && <nav>
+          <Link to = '/'><button>Home</button></Link>
+          <Link to = '/signup'><button>Register</button></Link>
+          <Link to = '/login'><button>Login</button></Link>
+          <div className='cart-btn'>
+            <img src = {Cartlogo} alt ='no img'/>
             <span className='cart-icon-css'>0</span>
-        </div>
-        </Link>
-        <Link to = '/userprofile'>
-        <img src = {Profilelogo} className='profile-icon' alt =""/>
-        </Link>
-      </nav>
+          </div>
+          <Link to = '/userprofile'>
+            <img src ={Profilelogo} className='profile-icon'/>
+          </Link>
+
+          </nav>}
+          {loggedUser &&
+          <nav>
+            <Link to = '/'><button>Home</button></Link>
+            <div className='cart-btn'>
+              <img src = {Cartlogo} alt = 'no logo'/>
+              <span className='cart-icon-css'>{loggedUser[0].cart}</span>
+            </div>
+            <Link to = '/userprofile'>
+              <img src ={Profilelogo} className='profile-icon'/>
+            </Link>
+            <button className='logout-btn' onClick={handleLogout}>
+              Logout
+            </button>
+          </nav>
+          }
+      </div>
+        
     </div>
   )
 }
